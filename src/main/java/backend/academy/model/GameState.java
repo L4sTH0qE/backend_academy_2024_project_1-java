90db9aea7566b0cbeea0b85e34b4bd9696e0ee3f
Alexander <avsolovkin@edu.hse.ru>
2024-09-26 22:54:15 +0300

    fix(code): Make hotfix for checkLetter() method in GameState

--- src/main/java/backend/academy/model/GameState.java
+++ src/main/java/backend/academy/model/GameState.java
@@ -45,19 +45,19 @@ public class GameState {
 
     /// Метод для проверки, была ли буква уже угадана.
     public boolean tryGuessLetter(char ch) {
-        char letter = ch >= 'a' && ch <= 'z' ? (char) (ch - 'a' + 'A') : ch;
+        char letter = (ch >= 'a' && ch <= 'z') ? (char) (ch - 'a' + 'A') : ch;
         return guessedLetters[letter - 'A'] != 1;
     }
 
     /// Метод для проверки нахождения буквы в слове.
     public boolean checkLetter(char ch) {
-        char letter = ch >= 'a' && ch <= 'z' ? (char) (ch - 'a' + 'A') : ch;
+        char letter = (ch >= 'a' && ch <= 'z') ? (char) (ch - 'a' + 'A') : ch;
         guessedLetters[letter - 'A'] = 1;
         boolean flag = false;
         for (int i = 0; i < correctWord.length(); ++i) {
-            if (correctWord.charAt(i) == ch) {
+            if (correctWord.charAt(i) == letter) {
                 flag = true;
-                currentWord[i] = ch;
+                currentWord[i] = letter;
             }
         }
         if (!flag) {
